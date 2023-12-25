f2da09ab44b7a9cbe772ff67ce5a6086516e2cec
ɯ̹t͡ɕʲi <xc18tx@gmail.com>
2023-12-26 02:45:45 +0900

    update clean command to add file deletion features

--- internal/adapter/controller/clean.go
+++ internal/adapter/controller/clean.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"fmt"
+	"os"
+	"path"
 
 	"github.com/c18t/nippo-cli/internal/core"
 	"github.com/spf13/cobra"
@@ -17,6 +19,31 @@ func NewCleanController() CleanController {
 }
 
 func (c *cleanController) Exec(cmd *cobra.Command, args []string) error {
-	fmt.Println("clean called")
+	fmt.Print("clean cache files... ")
+
+	clearNippoCache()
+	clearBuildCache()
+
+	fmt.Println("ok.")
+	return nil
+}
+
+func clearNippoCache() error {
+	outputDir := path.Join(core.Cfg.GetCacheDir(), "md")
+	files, err := os.ReadDir(outputDir)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		fileName := file.Name()
+		err = os.Remove(path.Join(outputDir, fileName))
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 	return nil
 }
