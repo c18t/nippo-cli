1b782f76bb201cd78379a67ba5b4e03e3690f59e
ɯ̹t͡ɕʲi <xc18tx@gmail.com>
2023-12-26 02:49:45 +0900

    update deploy command to support asset deployment
    - Update the deploy command to support deploying assets, such as images, CSS, and JavaScript, to the production environment.

--- internal/adapter/controller/deploy.go
+++ internal/adapter/controller/deploy.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"os/exec"
 	"path"
 
@@ -21,7 +23,37 @@ func NewDeployController() DeployController {
 
 func (c *deployController) Exec(cmd *cobra.Command, args []string) error {
 	fmt.Print("deploy to vercel... ")
+	dataDir := path.Join(core.Cfg.GetDataDir(), "output")
 	outputDir := path.Join(core.Cfg.GetCacheDir(), "output")
+
+	files, err := os.ReadDir(dataDir)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		src, err := os.Open(path.Join(dataDir, file.Name()))
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		defer src.Close()
+		dest, err := os.Create(path.Join(outputDir, file.Name()))
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		defer dest.Close()
+		_, err = io.Copy(dest, src)
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+	}
+
 	log, err := exec.Command("vercel", "--cwd", outputDir, "--prod").Output()
 	if err != nil {
 		fmt.Printf("err: %v\ndeploy log:\n%v", err, log)
