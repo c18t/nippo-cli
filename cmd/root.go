de1748b4f502f54682f3ab9b46338d8207c0e368
ɯ̹t͡ɕʲi <xc18tx@gmail.com>
2023-12-28 23:47:17 +0900

    fix --config message

--- cmd/root.go
+++ cmd/root.go
@@ -35,7 +35,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&root.Params().ConfigFile, "config", "", "config file (default is $XDG_CONFIG_HOME/nippo/.nippo.toml)")
+	rootCmd.PersistentFlags().StringVar(&root.Params().ConfigFile, "config", "", "config file (default is $XDG_CONFIG_HOME/nippo/nippo.toml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
